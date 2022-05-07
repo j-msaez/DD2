cc8ee1fb76cd0616ca9f36eb9bb32760b8032eff
j-msaez <j.msaez@alumnos.upm.es>
2022-05-08 01:00:15 +0200

    Hito-2 v2.3 Controlador modelado y testbench
    - Cambios:
      - (New) Automata modelado
      - (New) Testbench "funcional"
    
    - To Do:
      - Revisar "calc_offset.vhd" -> Completar modelo.
      - Revisar "estimador.vhd"   -> Completar modelo.
      - Revisar modelo del controlador.
      - Investigar como conectar calc_offset y estimador con el controlador.

--- hdl/spi_controlador.vhd
+++ hdl/spi_controlador.vhd
@@ -42,8 +42,8 @@ end entity;
 architecture rtl of spi_controlador is
 ---- ¿Automata para manejar que hacer en cada momento?
 ---- Estado del gestor del bus y signals de control derivadas
---  type t_estado is (initialization, configure, measure);
---  signal estado: t_estado;
+  type t_estado is (initialization, configure, measure);
+  signal estado: t_estado;
   
 -- 2.6.3 High resolution, normal mode, low-power mode - Table 9. Operating mode selection 
 -- Normal mode: Turn-on time [ms] -> 1.6 ms ~ 2 ms (margen de seguridad)
@@ -56,36 +56,70 @@ architecture rtl of spi_controlador is
   signal cnt_t_meas: std_logic_vector(17 downto 0);   -- Contador para generar la temporizacion de la realizacion de medidas (250000 -> 18 bits)
   signal cnt_t_ref:  std_logic_vector(5 downto 0);    -- Contador para generar la temporizacion del calculo del promedio (320 ms / 5 ms = 64 -> 6 bits).
   
-  signal reg_pos_x_ini: std_logic_vector(9 downto 0); -- Registro para almacenar la posicion inicial, en el eje X, de la tarjeta.
-  signal reg_pos_y_ini: std_logic_vector(9 downto 0); -- Registro para almacenar la posicion inicial, en el eje Y, de la tarjeta.
+--  signal reg_pos_x_ini: std_logic_vector(9 downto 0); -- Registro para almacenar la posicion inicial, en el eje X, de la tarjeta.
+--  signal reg_pos_y_ini: std_logic_vector(9 downto 0); -- Registro para almacenar la posicion inicial, en el eje Y, de la tarjeta.
   
+  signal cnt_conf_op: std_logic_vector(1 downto 0);
   
 begin
---  -- ¿Automata para manejar que hacer en cada momento?
---  -- Maquina de estados para el controlador.
---  process(clk, nRst)
---  begin
---    if nRst = '0' then
---      estado <= initialization;
--- 
---    elsif clk'event and clk = '1' then
---      case estado is
---        when initialization =>          -- A la espera de la entrada en operacion del sensor.
---          if meas_ena = '1' then
---            estado <= configure;
---            
---          end if;
---          
---        when configure =>               -- Realizar las configuraciones del esclavo.
---          estado <= measure;
--- 
---        when measure =>                 -- Operacion de escritura/lectura.
---          tipo_op_nW_R <= '1';
---          reg_addr     <= X"28";
---      end case;
---
---    end if;
---  end process;
+  
+  -- ¿Automata para manejar que hacer en cada momento?
+  -- Maquina de estados para el controlador.
+  process(clk, nRst)
+  begin
+    if nRst = '0' then
+      estado <= initialization;
+      tipo_op_nW_R <= '0';
+      reg_addr     <= (others => '0');
+      dato_wr      <= (others => '0');
+      
+    elsif clk'event and clk = '1' then
+      case estado is
+        when initialization =>          -- A la espera de la entrada en operacion del sensor.
+          if meas_ena = '1' then
+            estado <= configure;
+            
+            tipo_op_nW_R <= '0';        -- Write Operation.
+            reg_addr     <= "100011";   -- CTRL_REG4 register (0x23) -> BDU BLE FS1 FS0  HR ST1 ST0 SIM 
+            dato_wr      <= X"80";      -- CTRL_REG4 value    (0x80) ->  1   0   0   0   0   0   0   0  
+            
+          end if;
+          
+        when configure =>               -- Realizar las configuraciones del esclavo.
+          if cnt_conf_op = 1 then
+            tipo_op_nW_R <= '0';        -- Write Operation.
+            reg_addr     <= "100000";   -- CTRL_REG1 register (0x20) -> ODR3 ODR2 ODR1 ODR0 LPen Zen Yen Xen
+            dato_wr      <= X"63";      -- CTRL_REG1 value    (0x63) ->  0    1    1    0    0    0   1   1 
+            
+          elsif cnt_conf_op = 2 then
+            estado <= measure;
+            
+          end if;
+          
+        when measure =>                 -- Operacion de escritura/lectura.
+          tipo_op_nW_R <= '1';          -- Read Operation.
+          reg_addr     <= "101000";     -- OUT_X_L register (0x28)
+
+      end case;
+
+    end if;
+  end process;
+
+  -- Control de cnt_conf_op.
+  process(clk, nRst)
+  begin
+    if nRst = '0' then
+      cnt_conf_op <= (others => '0');
+      cnt_t_ref   <= (others => '0');
+      
+    elsif clk'event and clk = '1' then
+      if ini_tx = '1' and estado = configure then
+        cnt_conf_op <= cnt_conf_op + 1;
+
+      end if;
+
+    end if;
+  end process;
   
   -- Control de cnt_t_meas.
   process(clk, nRst)
@@ -95,7 +129,7 @@ begin
       meas_ena <= '0';
       
     elsif clk'event and clk = '1' then
-      if meas_ena = '1' then              -- Si la señal meas_ena no está activa, no se deben realizar lecturas.
+      if meas_ena = '1' and estado = measure then           -- Si la señal meas_ena no está activa, no se deben realizar lecturas.
         if cnt_t_meas < MEAS_T_CLK then
           cnt_t_meas <= cnt_t_meas + 1;
           
@@ -104,13 +138,13 @@ begin
           
         end if;
 
-      else                                -- Reutilizamos el contador para realizar la espera inicial de 2 ms.
+      elsif meas_ena = '0' and estado = initialization then -- Reutilizamos el contador para realizar la espera inicial de 2 ms.
         if cnt_t_meas < INIT_T_CLK then
           cnt_t_meas <= cnt_t_meas + 1;
           
         else 
-          cnt_t_meas <= (others => '0');  -- Finalizada la espera, se resetea el contador y
-          meas_ena <= '1';                -- Se activa la señal meas_ena.
+          cnt_t_meas <= (others => '0');                    -- Finalizada la espera, se resetea el contador y
+          meas_ena <= '1';                                  -- Se activa la señal meas_ena.
           
         end if;
         
@@ -118,8 +152,8 @@ begin
     end if;
   end process;
   
---  ini_tx <= '1' when (etado = measure   and meas_ena = '1' and cnt_t_meas = MEAS_T_CLK) and ready_tx = '1' else
---            '1' when (etado = configure and meas_ena = '1')                             and ready_tx = '1' else
---            '0';
+  ini_tx <= '1' when (estado = measure   and meas_ena = '1' and cnt_t_meas = MEAS_T_CLK) and ready_tx = '1' else
+            '1' when (estado = configure and meas_ena = '1')                             and ready_tx = '1' else
+            '0';
 
 end rtl;
---
+++ test/test_top_controlador_spi.vhd
@@ -0,0 +1,122 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity test_top_controlador_spi is
+end entity;
+
+architecture test of test_top_controlador_spi is
+
+-- Segnales del DUT
+  signal clk:          std_logic;
+  signal nRst:         std_logic;
+  signal pos_X:        std_logic_vector(1 downto 0);
+  signal pos_Y:        std_logic_vector(1 downto 0);
+  signal nCS:          std_logic;
+  signal SPC:          std_logic;
+  signal MISO:         std_logic;
+  signal MOSI:         std_logic;
+  signal ini_tx:       std_logic;
+  signal tipo_op_nW_R: std_logic;
+  signal reg_addr:     std_logic_vector(5 downto 0);
+  signal dato_wr:      std_logic_vector(7 downto 0);
+  signal ena_rd:       std_logic;
+  signal dato_rd:      std_logic_vector(7 downto 0);
+  signal ready_tx:     std_logic;
+  
+  constant Tclk:       time := 20 ns; -- reloj de 50 MHz
+  
+  begin
+
+  -- Reloj de 50 MHz
+
+  process
+  begin
+    clk <= '0';
+    wait for Tclk/2;
+    clk <= '1';
+    wait for Tclk/2;
+  end process;
+  
+  -- Esclavo SPI
+  agente_spi: entity work.agente_spi(sim)
+    port map(pos_X => pos_X,
+             pos_Y => pos_Y,
+             nCS   => nCS,
+             SPC   => SPC,
+             SDI   => MOSI,
+             SDO   => MISO);
+
+  -- DUT
+  spi_master: entity work.spi_master(rtl)
+    port map(clk          => clk         ,
+             nRst         => nRst        ,
+             nCS          => nCS         ,
+             SPC          => SPC         ,
+             MISO         => MISO        ,
+             MOSI         => MOSI        ,
+             ini_tx       => ini_tx      ,
+             tipo_op_nW_R => tipo_op_nW_R,
+             reg_addr     => reg_addr    ,
+             dato_wr      => dato_wr     ,
+             ena_rd       => ena_rd      ,
+             dato_rd      => dato_rd     ,
+             ready_tx     => ready_tx);
+             
+  -- DUT
+  spi_controlador: entity work.spi_controlador(rtl)
+    port map(clk          => clk         ,
+             nRst         => nRst        ,
+             ini_tx       => ini_tx      ,
+             tipo_op_nW_R => tipo_op_nW_R,
+             reg_addr     => reg_addr    ,
+             dato_wr      => dato_wr     ,
+             ena_rd       => ena_rd      ,
+             dato_rd      => dato_rd     ,
+             ready_tx     => ready_tx);
+  
+-- Secuencia de estimulos
+
+  process
+  begin
+    report "*********************************************TEST: comienza el test";
+    report "*********************************************TEST: comienza el reset asincrono";
+    -- Reset
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    nRst <= '1';
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    
+    nRst <= '0';
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    pos_X <= "00";
+    pos_Y <= "00";
+    
+    
+    wait until clk'event and clk = '1';
+    wait until clk'event and clk = '1';
+    nRst <= '1';
+    -- Fin de reset
+    
+    wait for 25 ms;
+    wait until clk'event and clk = '1';
+    pos_X <= "10";
+    pos_Y <= "10";
+    
+    wait for 25 ms;
+    wait until clk'event and clk = '1';
+    pos_X <= "11";
+    pos_Y <= "11";
+    
+    wait for 25 ms;
+    wait until clk'event and clk = '1';
+  
+    -- Fin del test
+    assert false
+    report "fin del test de MEDTH"
+    severity failure;
+
+  end process;
+end test;
